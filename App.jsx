38a8bb21149e38282af5a744d996f7c4eec6418c
DharshiniCS <146570851+DharshiniCS@users.noreply.github.com>
2026-06-24 19:21:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import APIIntegration from './Task_12/APIIntegration'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <APIIntegration/>
+    </div>
+  )
+}
